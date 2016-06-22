39807da94818ed6962eccebc759d731c672c46ba
gool <ss22219@qq.com>
2016-06-22 09:06:22 +0000

    omnisharp-vim

--- vim-install-plugin.sh
+++ vim-install-plugin.sh
@@ -2,3 +2,5 @@ if [ ! -d ~/.vim/bundle/Vundle.vim ];then
 	git clone https://github.com/VundleVim/Vundle.vim.git ~/.vim/bundle/Vundle.vim
 fi
 vim +PluginInstall +qall
+cd ~/.vim/bundle/omnisharp-vim/server/
+xbuild
